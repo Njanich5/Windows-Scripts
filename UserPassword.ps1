0e58bd5dd9eb69af14ebe7807de58d53c4a51c4b
Nicholas Janich <njanich5@gmail.com>
2020-03-13 12:26:06 -0500

    Update UserPassword.ps1

--- UserPassword.ps1
+++ UserPassword.ps1
@@ -1,3 +1,18 @@
+#Powershell Script to enter a username then check if the AD password is expired within a 90 day password policy
+
 $User = Read-Host -Prompt 'Input the user name'
-Get-ADUser -Identity “$User” -Properties “PasswordLastSet”
-Read-Host -Prompt “Press Enter to exit”
+Get-ADUser -Identity â€œ$Userâ€ -Properties â€œPasswordLastSetâ€
+
+$aduserprops = Get-ADUser -Identity â€œ$Userâ€ -Properties "PasswordLastSet"
+$username = ($aduserprops.GivenName, $aduserprops.Surname)
+$pwdate = $aduserprops.PasswordLastSet 
+$date = Convert-TimeToDays($pwdate)
+$DaysTillExpired = (90 - $date)
+
+if ($DaysTillExpired -le 3)
+{ $PositiveDays = [math]::abs($DaysTillExpired)
+Write-Host "$username's is expired $PositiveDays days ago and needs to be changed" }
+else {
+Write-Host "$username's Password will expire in $DaysTillExpired Days"
+}
+Read-Host -Prompt â€œPress Enter to exitâ€
